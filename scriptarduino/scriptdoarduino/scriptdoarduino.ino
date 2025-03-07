5650fe9a846c372067e78a665aeda0abdfce72df
tiago <tiago@localhost.localdomain>
2025-03-07 07:21:26 -0300

    arrumei erros basicos, que deve funcionar em qualquer pc, espero eu

--- scriptarduino/scriptdoarduino/scriptdoarduino.ino
+++ scriptarduino/scriptdoarduino/scriptdoarduino.ino
@@ -1,7 +1,7 @@
 #define sim 2
 #define nao 3
 #define nao_sei 4
-
+#define max_tempo 1000
 void setup() {
   // put your setup code here, to run once:
   pinMode(sim, INPUT);
@@ -12,21 +12,31 @@ void setup() {
 
 void loop() {
   // Defino que o arduino deve ler estas conexões com os valores digitais, ou seja, 1 (ligado), 0 (desligo)
-  int botao_sim = digitalRead(sim);
-  int botao_nao = digitalRead(nao);
-  int botao_nao_sei = digitalRead(nao_sei);
-  if (botao_sim == HIGH){ // Se o botão for clicado, envia para a porta serial o seguinte valor
-    Serial.println("sim"); // Valor a ser enviado
-    delay(1000);
+  // int botao_sim = digitalRead(sim);
+  // int botao_nao = digitalRead(nao);
+  // int botao_nao_sei = digitalRead(nao_sei);
+  int botoes[] = {sim,nao,nao_sei};
+  char *repostas[] = {"sim", "nao", "nao_sei"};
+  int tamanho = 3;
+  for (int i = 0;i<tamanho;i++){
+    if(digitalRead(botoes[i]) == HIGH){
+      Serial.println(repostas[i]);
+      delay(max_tempo);
+      break;
+    }
   }
+  // if (botao_sim == HIGH){ // Se o botão for clicado, envia para a porta serial o seguinte valor
+  //   Serial.println("sim"); // Valor a ser enviado
+  //   delay(1000);
+  // }
 
-  if (botao_nao == HIGH){ // Se o botão for clicado, envia para a porta serial o seguinte valor
-    Serial.println("nao"); // Valor a ser enviado
-    delay(1000);
-  }
+  // if (botao_nao == HIGH){ // Se o botão for clicado, envia para a porta serial o seguinte valor
+  //   Serial.println("nao"); // Valor a ser enviado
+  //   delay(1000);
+  // }
   
-  if (botao_nao_sei == HIGH){ // Se o botão for clicado, envia para a porta serial o seguinte valor
-    Serial.println("nao_sei"); // Valor a ser enviado
-    delay(1000);
-  }
+  // if (botao_nao_sei == HIGH){ // Se o botão for clicado, envia para a porta serial o seguinte valor
+  //   Serial.println("nao_sei"); // Valor a ser enviado
+  //   delay(1000);
+  // }
 }
--- sistema.py
+++ sistema.py
@@ -5,30 +5,44 @@ import time
 
 respostas = []
 erro = None
+porta = "/dev/ttyUSB0"
+baud_rate = 9000
+
 
 def conexao_arduino():
-    '''
+    """
     Esta função estabelece a comunicação com o arduíno, assim como fica lendo a porta(USB) em que ele esta conectado
     continuadamente pegar os valores dos buttons quando eles são pressionados
 
     Manti a conexão assim como a leitura junto para não precisar ficar fazendo várias funções
-    '''
+    """
     global respostas
-    porta = os.system('python -m serial.tools.list_ports') # manda um comando para o SO para retornar a porta que o arduíno está conectado
-    try:
-        arduino = serial.Serial(porta) # Cria o objeto que vai manipular esta porta e passa como argumento a porta que ele vai manipular
-        while True: # loop contínuo para ler a porta
-            if arduino.in_waiting(): # caso haja algum valor na porta
-                respostas.append(arduino.readline()) # armazena esta informação
-            time.sleep(0.5)
-    except Exception as erro:
-        print(erro)
+    # porta = os.system('python -m serial.tools.list_ports') # manda um comando para o SO para retornar a porta que o arduíno está conectado, nao retorna a
+    # porta corretam retorna um object int
+    # a conexao com o arduino nao pode tar dentro do try_catch, sei la o porque, so não pode pq decodifica errado, nao do padrao utf-8
+    arduino = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
+    print("Conectado ao Arduino na porta /dev/ttyUSB0")
 
-sistema = Flask(__name__,template_folder='./templates')
+    while True:
+        if arduino.in_waiting > 0:  # Se houver dados na porta serial
+            dados = arduino.readline()  # Lê uma linha completa da serial
+            try:
+                # Tenta decodificar com UTF-8
+                dados_decodificados = dados.decode("utf-8").strip()  # UTF-8
+                print(f"Recebido: {dados_decodificados}")
+                respostas = respostas.append(dados_decodificados)
+            finally:
+                print("")
 
-@sistema.route('/')
+
+sistema = Flask(__name__, template_folder="./templates")
+
+
+@sistema.route("/")
 def ola():
-    return render_template('perguntas.html')
+    return render_template("perguntas.html")
 
-if __name__ == '__main__':
-    sistema.run(debug=  True)
+
+if __name__ == "__main__":
+    sistema.run(debug=True)
+conexao_arduino()
