aa63b61b7cedac52dc4597f1d7527f2ed84afcc9
tiago <tiago@localhost.localdomain>
2025-03-17 15:01:02 -0300

    preparando o terrano para ficar mais facil de implementar depois e arrumando algumas coisas

--- websocket/main.go
+++ websocket/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -16,6 +15,22 @@ import (
 
 type repostasdb struct {
 	DB *sql.DB
+}
+
+type PerguntaResposta struct {
+	ID       int    `json:"id"`
+	Resposta string `json:"resposta"`
+	Pergunta string `json:"pergunta"`
+}
+
+type Data struct {
+	Tipo string `json:"type"`
+	Msg  string `json:"msg"`
+}
+
+type Data_log struct {
+	Name string
+	Date *[]PerguntaResposta
 }
 
 func (r *repostasdb) getbyid(id int) (*PerguntaResposta, error) {
@@ -32,24 +47,32 @@ func (r *repostasdb) getbyid(id int) (*PerguntaResposta, error) {
 	return &pergunta, nil
 }
 
-type PerguntaResposta struct {
-	ID       int    `json:"id"`
-	Resposta string `json:"resposta"`
-	Pergunta string `json:"pergunta"`
-}
-
 var addr = flag.String("addr", "localhost:8080", "http service address")
 var upgrader = websocket.Upgrader{}
 var exaplechh chan string
 var respostas = make([]PerguntaResposta, 0)
 
-func associar_id_pergunta_resposta(id int, pergunta string, resposta string, db *repostasdb) (*PerguntaResposta, int) {
+func perguta_inicial(db *repostasdb) (*PerguntaResposta, int) {
+	perg, err := db.getbyid(1)
+	if err != nil {
+		log.Printf("deu algum erro ao receber a pergunta: %v", err)
+		return nil, 0
+	}
+	return perg, 2
+}
+
+func associar_id_pergunta_resposta(id int, pergunta string, resposta string, db *repostasdb) (*PerguntaResposta, int, bool) {
 	// Crio o objeto resposta
 	pr := PerguntaResposta{
 		ID:       id,
 		Pergunta: pergunta,
 		Resposta: resposta,
 	}
+	// a gente ainda pode usar essa funçao para verificar quantas vezes o usuario respodeu nao_sei
+	// e com isso a gente pode setar o terminou como true e parar este usuario
+
+	// variavel para verificar se as perguntas terminaram
+	var terminou bool
 	// verifica no banco de dados se tem mais uma perguta
 	nova_pergunta, err := db.getbyid(id)
 	// se dar um erro entao nao existe mais pergunta, entao ele so fica reatribuindo valores para o
@@ -57,7 +80,7 @@ func associar_id_pergunta_resposta(id int, pergunta string, resposta string, db 
 	if err != nil {
 		// reseta o id renferenciado, para 1
 		id = 1
-
+		terminou = true
 		nova_pergunta, _ = db.getbyid(id)
 	} else if len(respostas) < id {
 		// aqui ele verifica se o tamanho do array e menor que o id, se for ele vai adicionar o novo objeto no array
@@ -69,13 +92,43 @@ func associar_id_pergunta_resposta(id int, pergunta string, resposta string, db 
 		id++
 	}
 	// retorna a nova pergunta, sem resposta
-	return nova_pergunta, id
+	return nova_pergunta, id, terminou
+}
+
+func enviar_message(msg []byte, ws *websocket.Conn) error {
+	return ws.WriteJSON(msg)
+}
+
+func covertdatatojson(tipo string, p *PerguntaResposta, s string) ([]byte, error) {
+	var d Data
+	if p == nil {
+		d = Data{
+			Tipo: tipo,
+			Msg:  s,
+		}
+	} else {
+		d = Data{
+			Tipo: tipo,
+			Msg:  p.Pergunta,
+		}
+	}
+	jsondata, err := json.Marshal(d)
+	if err != nil {
+		return []byte(""), nil
+	}
+	return jsondata, nil
+}
+
+// funçao para criar o arquvo com as repostas
+// tambem precisa verificar se e para colocar no banco de dados como um log, dai eu preciso pegar o path
+func criar_o_arquivo(d Data_log) error {
+
+	return nil
 }
 
 // inicia o websocket
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
-	var id int = 1
 	if err != nil {
 		log.Print("Upgrade error:", err)
 	}
@@ -84,26 +137,41 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		log.Print("Upgrade error:", err)
 		return
 	}
-	var perg *PerguntaResposta
-	perg, err = repdb.getbyid(id)
+	perg, id := perguta_inicial(repdb)
+	jsondata, err := covertdatatojson("string", perg, "")
 	if err != nil {
 		log.Println(err)
-	} else {
-		respostas = append(respostas, *perg)
 	}
-	err = c.WriteMessage(websocket.TextMessage, []byte(perg.Pergunta))
-	if err != nil {
-		log.Println("Write error:", err)
-	}
+	log.Println(enviar_message(jsondata, c))
 	id++
+	var terminou bool = false
 	for {
 		// ler o chanal, e caso tiver messagem envia para o cliente WebSocket
 		select {
 		case message := <-exaplechh:
-			perg, id = associar_id_pergunta_resposta(id, perg.Pergunta, message, repdb)
-			err = c.WriteMessage(websocket.TextMessage, []byte(perg.Pergunta))
-			if err != nil {
-				log.Println(err)
+			if terminou {
+				terminou = false
+				jsondata, err := covertdatatojson("html", nil, "sei la depois a gente pensa melhor nisso")
+				if err != nil {
+					log.Println(err)
+				}
+				log.Println(enviar_message(jsondata, c))
+				_, message, err := c.ReadMessage()
+				if err != nil {
+					log.Println(err)
+				}
+				file_conteudo := Data_log{
+					Name: string(message),
+					Date: &respostas,
+				}
+				log.Println(criar_o_arquivo(file_conteudo))
+			} else {
+				perg, id, terminou = associar_id_pergunta_resposta(id, perg.Pergunta, message, repdb)
+				jsondata, err := covertdatatojson("string", perg, "")
+				if err != nil {
+					log.Println(err)
+				}
+				log.Println(enviar_message(jsondata, c))
 			}
 		default:
 			time.Sleep(time.Second * 2)
@@ -136,7 +204,6 @@ func abrir_banco_dados() (*repostasdb, error) {
 	fmt.Println("aqui")
 	return &repostasdb{DB: db}, nil
 }
-
 func main() {
 	exaplechh = make(chan string)
 	flag.Parse()
